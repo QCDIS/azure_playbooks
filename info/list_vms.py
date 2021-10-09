c1478b32abd85e3aaef02283bae458936e613cb6
skoulouzis <koulouzis@gmail.com>
2021-10-09 17:54:06 +0200

    rename

--- info/list_vms.py
+++ info/list_vms.py
@@ -3,10 +3,8 @@
 from azure.common.credentials import ServicePrincipalCredentials
 from azure.mgmt.compute import ComputeManagementClient
 
-
 import json
 import sys
-
 
 from azure.common.credentials import ServicePrincipalCredentials
 from azure.mgmt.compute import ComputeManagementClient
@@ -19,7 +17,6 @@ if __name__ == '__main__':
     azure_tenant = sys.argv[4]
     region = sys.argv[5]
 
-
     credentials = ServicePrincipalCredentials(
         client_id=azure_client_id,
         secret=azure_secret,
@@ -29,13 +26,11 @@ if __name__ == '__main__':
     compute_client = ComputeManagementClient(credentials, azure_subscription_id)
     virtual_machine_sizes = []
     for vm_size in compute_client.virtual_machine_sizes.list(region):
-        dict_vm_size = {}
-        dict_vm_size['name'] = vm_size.name
-        dict_vm_size['max_data_disk_count'] = vm_size.max_data_disk_count
-        dict_vm_size['memory_in_mb'] = vm_size.memory_in_mb
-        dict_vm_size['number_of_cores'] = vm_size.number_of_cores
-        dict_vm_size['os_disk_size_in_mb'] = vm_size.os_disk_size_in_mb
-        dict_vm_size['resource_disk_size_in_mb'] = vm_size.resource_disk_size_in_mb
+        dict_vm_size = {'name': vm_size.name, 'max_data_disk_count': vm_size.max_data_disk_count,
+                        'memory_in_mb': vm_size.memory_in_mb, 'number_of_cores': vm_size.number_of_cores,
+                        'os_disk_size_in_mb': vm_size.os_disk_size_in_mb,
+                        'resource_disk_size_in_mb': 10000}
+                        # 'resource_disk_size_in_mb': vm_size.resource_disk_size_in_mb}
         virtual_machine_sizes.append(dict_vm_size)
 
     print(json.dumps(virtual_machine_sizes))
